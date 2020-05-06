70148d5e42a78987a0818118d210c99bc26c0024
AGUDO ALONSO Alio <alioagudo@gmail.com>
2020-05-06 13:37:36 +0200

    feature: MovieCard component created

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <h1>{{movie ? movie.Title : ''}}</h1>
+    <img :src= getPoster()>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['movie'],
+  created: function() {
+
+  },
+  methods: {
+    getPoster (){
+      return this.movie.Poster === 'N/A' ? './no-image.png' : this.movie.Poster;
+    }
+  }
+
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

2dbefa980852e04327af1ca2b95a193fae8f895e
AGUDO ALONSO Alio <alioagudo@gmail.com>
2020-05-06 13:37:01 +0200

    feature: inputSearch component created

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="input-container">
+    <input type="text" v-on:keyup="$emit('inputChanges', $event.target.value)" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+  .input-container{
+    background-color: yellow;
+    height: 110px;
+  }
+</style>

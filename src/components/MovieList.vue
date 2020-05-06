f4a36b00088fe1e265a9c15723e7ce10333ef9fd
AGUDO ALONSO Alio <alioagudo@gmail.com>
2020-05-06 13:37:25 +0200

    feature: MovieList component crated

---
+++ src/components/MovieList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="list">
+    <movie-card v-for="(movie) in movies" :key="movie.Imdb" v-bind:movie=movie></movie-card>
+  </div>
+</template>
+
+<script>
+import MovieCard from './MovieCard.vue'
+
+
+export default {
+  props: {
+    movies: Array
+  },
+  components: {
+    MovieCard
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>

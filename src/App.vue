a0fe0ae499b78b7a28e0a35d1de6bcec5bb36a65
AGUDO ALONSO Alio <alioagudo@gmail.com>
2020-05-06 13:37:55 +0200

    feature: main view configurated

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InputSearch @inputChanges="inputChanges"></InputSearch>
+    <MovieList v-bind:movies="mutableList" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MovieList from "./components/MovieList.vue";
+import InputSearch from "./components/InputSearch.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MovieList,
+    InputSearch
+  },
+  data: function() {
+    return {
+      inputValue: "Spiderman",
+      page: 1,
+      type: "movie",
+      mutableList: []
+    };
+  },
+  props: {
+    movies: {
+      type: Array
+    }
+  },
+  created: function() {
+    this.getMovies();
+  },
+  methods: {
+    setMovies(movies) {
+      this.movies = movies;
+    },
+    inputChanges(newInput) {
+      this.inputValue = newInput;
+      this.getMovies();
+    },
+    getMovies() {
+      fetch(
+        `https://www.omdbapi.com/?s=${this.inputValue}&plot=full&apikey=e477ed6a&page=${this.page}&type=${this.type}`
+      )
+        .then(response => {
+          return response.json();
+        })
+        .then(result => {
+          if (result.Search && result.Search.length > 0) {
+            this.mutableList = result.Search;
+          } else {
+            this.mutableList = [];
+          }
+        })
+        .catch(err => {
+          console.log("Se ha producido un error: " + err);
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :host {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 0;
+    padding: 0;
+  }
 </style>
